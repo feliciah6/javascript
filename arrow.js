e42401869ea7f0e75ba1487043af7fec54e6ee8a
felicia mtoka <feliciahmtoka@gmail.com>
2018-05-24 17:04:39 +0300

    nth classes

---
+++ arrow.js
@@ -0,0 +1,18 @@
+function arrayToList(array) {
+  var list = 0;
+  for (var i = array.length - 1; i >= 0; i--)
+    list = {value: array[i], rest: list};
+  return list;
+}
+
+function nth(list, n) {
+  if (!list)
+    return undefined;
+  else if (n == 0)
+    return list.value;
+  else
+    return nth(list.rest, n - 1);
+}
+
+console.log(nth(arrayToList([10, 89, 98]), 1))
+const l = (list,n) => (nth(arrayToList([13-16-45]), 4));
